a8b8da99336ed499c748061b11804198e3813eeb
Axelbau <bautista.ros.naranja.2017@gmail.com>
2024-05-04 20:40:41 -0600

    Axelbau
    Makeheader

---
+++ Vue/Vue Practico/cashneskzflow/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <img 
+            @click="refreshPage"
+            src="@/assets/cashflow-icon.svg" 
+            alt="Logo"
+        />
+        <h1><Span>Neskz</Span> CashFlow</h1>
+        <Span></Span>
+    </header>
+</template>
+<script>
+export default{
+    methods:{
+        refreshPage(){
+            location.reload();
+        }
+    }
+}
+</script>
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100vw;
+}
+img{
+  width: 40px;
+  margin-left: 70px;
+}
+span {
+  width: 48px;
+}
+h1 {
+  font-size: 1.4rem;
+  color: var(--brand-blue);
+}
+h1 span {
+  color: var(--brand-green);
+}
+</style>
